ff6ef4fef3fe744861afe4fac8335c112155e08e
AAkinkunmi <aakinkunmi@pursuit.org>
2024-04-17 14:06:48 -0400

    fix file path for images

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./card.css"
+import person from "../card/person.jpg"
 
 const Card = (prop) => {
   return (
     <div className='card'>
       <div className="person">
-        <img src="./src/assets/person.jpg" alt="image" id='person'/>
+        <img src={person} alt="image" id='person'/>
       </div>
       <div className="name">
         <p>{prop.name}</p>
